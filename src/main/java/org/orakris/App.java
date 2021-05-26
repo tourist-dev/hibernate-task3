0a62a619a9879940d0690a30553b6d7daa64ba23
tourist-dev <shaik.sameer@orakris.com>
2021-05-26 14:39:43 +0530

    added starter code in App.java

--- src/main/java/org/orakris/App.java
+++ src/main/java/org/orakris/App.java
@@ -1,5 +1,13 @@
 package org.orakris;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure().build();
+        Metadata metadata = new MetadataSources(ssr).getMetadataBuilder().build();
+
+        SessionFactory sf = metadata.getSessionFactoryBuilder().build();
+        Session session = sf.openSession();
+
+        Transaction t = session.beginTransaction();
+        t.commit();
+        session.close();
+
     }
 }
